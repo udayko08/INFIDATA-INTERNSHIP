cc37d3a78be33023709370ecbaba785c36bea81c
uday <udaykumarko46698@gmail.com>
2024-04-18 12:09:22 +0530

    added matplotlib

---
+++ machine learning/matplotlib/2 plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt #for visualization
+import  numpy as np #for array operations
+x = np.array([0,6])
+y = np.array([0,10])
+
+plt.plot(x,y,'-o') #creating a plot
+plt.show()#displaying the plot
+# *
+# o
+#-
+#--
---
+++ machine learning/matplotlib/3 matplotlib.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array([0,6])
+y = np.array([0,10])
+
+plt.plot(x,y)
+plt.show()
